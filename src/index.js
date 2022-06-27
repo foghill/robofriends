9127a11ca4a04c0421bf90a02c4c02dd73c78f73
Benjamin <ben@thefoghill.com>
2022-06-27 16:12:30 -0400

    Cardlist Component and mapping cardArray

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
+import {robots} from './robots'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
