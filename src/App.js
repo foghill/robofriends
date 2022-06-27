831ea6457e63605a9b2a1e85d93f4a5f494c6d88
Benjamin <ben@thefoghill.com>
2022-06-27 17:38:56 -0400

    change from functional to class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
-import SearchBox from './SearchBox'
+import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
-const App = () => {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
 
-    function onSearchChange(event){
-        console.log(event.target.value)
-    }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-  return (
-    < div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox searchChange={onSearchChange} />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    console.log(filteredRobots);
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
