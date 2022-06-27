a72cdb47d67f19b3a108a148a9415ebbdfe325ee
Benjamin <ben@thefoghill.com>
2022-06-27 16:20:51 -0400

    added search box

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import App from './App'
 import 'tachyons'
-import {robots} from './robots'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />
   </React.StrictMode>
 );
 
